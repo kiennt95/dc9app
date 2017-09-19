723b11fc4f47e26dae26359346a270476e54e3f9
quanlee <quan.le350@gmail.com>
2017-09-19 16:22:11 +0700

    commit

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -44,11 +44,45 @@ class UserController extends Controller
     public function login(Request $request)
     {
       // login here
-        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+        $login = [];
+        if($request->password)
+            $login['password'] = $request->password;
 
+        if($request->email)
+            $login['email'] = $request->email;
+
+        if($request->username)
+            $login['username'] = $request->username;
+
+        if($request->phone)
+            $login['phone'] = $request->phone;
+
+        if (count($login)>1 && Auth::attempt($login)) {
             // The user is being remembered...
-            return Auth::user();
+            return response()->json([
+                'code'      => 200,
+                'mesenger'  =>'LOGIN_SUCCESS',
+                'data'      =>[
+                    'username'      => Auth::user()->username,
+                    'first_name'    => Auth::user()->first_name,
+                    'last_name'     => Auth::user()->last_name,
+                    'phone'         => Auth::user()->phone,
+                    'birthday'      => Auth::user()->birthday,
+                    'gender'        => Auth::user()->gender,
+                    'status'        => Auth::user()->status,
+                    'avatar'        => Auth::user()->avatar,
+                    'group_id'      => Auth::user()->group->id,
+                    'group_name'    => Auth::user()->group->name
+                ]
+            ]);
+
         }else{
+            return response()->json([
+                'code'      => 401,
+                'mesenger'  =>'LOGIN_FAIL',
+                'data'      =>[
+                ]
+            ]);
 
         }
     }

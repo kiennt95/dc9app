8a6b460ff5ae176fbb2e0979aee66de64a55a404
quanlee <quan.le350@gmail.com>
2017-09-20 15:20:15 +0700

    commit

--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,18 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::post('/login', 'Api\\UserController@login');
-Route::post('/logout', 'Api\\UserController@logout');
-Route::post('/register', 'Api\\UserController@register');
+// user
+Route::post('/v1/user/login', 'Api\\UserController@login');
+Route::post('/v1/user/logout', 'Api\\UserController@logout');
+Route::post('/v1/user/register', 'Api\\UserController@register');
+
+// Menu
+Route::get('/v1/menu', 'Api\\MenuController@index');
+Route::get('/v1/menu-detail/{id}', 'Api\\MenuController@detail');
+
+// service
+Route::get('/v1/service', 'Api\\ServiceController@index');
+Route::get('/v1/service-detail/{id}', 'Api\\ServiceController@detail');
 
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
